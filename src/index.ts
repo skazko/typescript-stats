3aff336c2e614c96845cfb0bcb4ea7f8210ed388
Slava <kreeon@ya.ru>
2020-04-13 18:53:24 +0300

    refactor with generic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hey');
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
+
+const reader = new MatchReader('original.csv');
+reader.read();
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United wins: ${manUnitedWins}`);
